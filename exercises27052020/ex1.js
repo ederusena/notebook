f74a19dd618d0d0d872fd9fbeb3f1b25d6268e8e
Eder Sena <eder.sena@live.com>
2020-05-27 11:10:32 -0300

    Create ex1.js
    https://www.youtube.com/watch?v=OCKbXAepZyk&list=PLnNURxKyyLIIyJ_XKZHzU8SQyGu7yuqhn&index=26

---
+++ exercises27052020/ex1.js
@@ -0,0 +1,7 @@
+// Escreva uma funcao que usa 2 parametros como
+// argumento e retorna o maior valor entre eles
+
+const maxValue = (a,b) => a > b ? a : b;
+let greaterThan = maxValue(1,5);
+
+console.log(greaterThan);
